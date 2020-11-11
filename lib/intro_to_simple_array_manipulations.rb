43fb4b6e0ffb298ab960362eee3de523f19651d6
Duncan Stark <letmunplaysgames@gmail.com>
2020-11-11 21:13:51 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -30,6 +30,7 @@ def using_concat(my_favorite_things, more_favs)
   end 
   
   def using_delete_at (array, Integer) 
+    Integer = 2
     array = ["Johnny 5", "R2D2", "Robocop"]
     array.delete_at(2)
   end
